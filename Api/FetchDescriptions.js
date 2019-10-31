29e6726554aab24bbd066fdd1d837ea12e922154
sandbornm <michael.sandborn@vanderbilt.edu>
2019-10-30 21:04:03 -0500

    db schema and api directory

---
+++ Api/FetchDescriptions.js
@@ -0,0 +1,13 @@
+// MongoDB entry schema:
+
+// {"item": "shirt",
+// "brand": "Nike",
+// "size": "M",
+// "type": "short sleeved",
+// "material": "90% polyester 10% cotton",
+// "care": "wash cold with like colors",
+// "color": "blue",
+// "pattern": "solid with white Nike swoosh on chest",
+// "combination suggestion":"wear with khaki shorts or pants or any athletic shorts",
+// "price": "24.95 USD",
+// "opacity": "solid, opaque"
